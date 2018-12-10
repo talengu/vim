7a7bc3e42a1b980a620ab63c8ac5e530ed9de252
talengu <talenapp@gmail.com>
2018-12-11 02:26:05 +0800

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -3,6 +3,7 @@
 # install from https://github.com/talengu/vim
 
 VIM_FOLDER=~/.vim
+URL_BASHRC=https://raw.githubusercontent.com/talengu/vim/master/talen.bashrc
 
 if [ -d $VIM_FOLDER ]; then
    echo "Folder $VIM_FOLDER exists."
@@ -10,7 +11,7 @@ else
   echo "Folder $VIM_FOLDER exists."
   mkdir $VIM_FOLDER
 fi
-curl https://github.com/talengu/vim/blob/master/talen.bashrc > $VIM_FOLDER/talen.bashrc
+curl $URL_BASHRC > $VIM_FOLDER/talen.bashrc
 echo "source $VIM_FOLDER/talen.bashrc" >> ~/.bashrc
 echo "set completion-ignore-case on" >> ~/.inputrc
 
